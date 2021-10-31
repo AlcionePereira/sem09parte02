ab22e504bc7d5c5e89699b81639d4c5bdb99e9af
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-30 21:06:27 -0300

    Add files via upload

---
+++ bugs11.py
@@ -0,0 +1,7 @@
+cont = 0
+pulo = 11 
+for i in range(99, 0, -11):
+    cont = i
+    print(str( cont) +' '+'bugs no software, pegue onze deles e conserte...\nTecle "Ctrl+F5"')
+print( "Sem erros no software! Está estabilizado!")
+        
