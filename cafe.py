ae003730634304cb175a449e3b17805129d0c51f
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-30 21:06:48 -0300

    Add files via upload

---
+++ cafe.py
@@ -0,0 +1,9 @@
+cont = 0
+for i in range(99, 251):
+    cont = i
+    print(str(cont) + ' '+'bugs no software, pegue um deles e conserte...')
+    print('Tecle "Ctrl+F5"')
+
+    
+print('Vamos fazer mais um café!')
+    
